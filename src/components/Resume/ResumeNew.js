9def2b27071950f0257b313f116b4512e960c3e6
UsamaBaig <usamabaig.skncoe.comp@gmail.com>
2024-12-04 13:08:21 +0530

    linkedn url changed in resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-import pdf1 from '../../Assets/Usama-Resume.pdf.pdf';
+import pdf1 from '../../Assets/Usama-Resume.pdf';
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
